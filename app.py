75385637e2cc13e86febf76839bc60328a0891dc
Alon Cohen <alon@placer.io>
2023-08-01 00:03:56 +0300

    Update app.py
    main app update

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask, request, render_template,jsonify
+import os
+import pandas as pd
+from whatstk import df_from_txt_whatsapp
+
+from flask import *
+
 app = Flask(__name__)
-def do_something(text1,text2):
-   text1 = text1.upper()
-   text2 = text2.upper()
-   combine = text1 + text2
-   return combine
+
+
 @app.route('/')
-def home():
-    return render_template('home.html')
-@app.route('/join', methods=['GET','POST'])
-def my_form_post():
-    text1 = request.form['text1']
-    word = request.args.get('text1')
-    text2 = request.form['text2']
-    combine = do_something(text1,text2)
-    result = {
-        "output": combine
-    }
-    result = {str(key): value for key, value in result.items()}
-    return jsonify(result=result)
+def main():
+    return render_template("index.html")
+
+
+@app.route('/success', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('data/'+f.filename)
+        print('data/'+f.filename)
+        df = df_from_txt_whatsapp('data/'+f.filename)
+        return df.to_html()
+        # return render_template("Acknowledgement.html", name=f.filename)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
