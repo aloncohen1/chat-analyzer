f685ec59ff800f5a08b4aca22b2c8484101a203c
Alon Cohen <alon@placer.io>
2023-07-31 23:46:39 +0300

    Update app.py
    app update

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template,jsonify
 app = Flask(__name__)
-
+def do_something(text1,text2):
+   text1 = text1.upper()
+   text2 = text2.upper()
+   combine = text1 + text2
+   return combine
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+@app.route('/join', methods=['GET','POST'])
+def my_form_post():
+    text1 = request.form['text1']
+    word = request.args.get('text1')
+    text2 = request.form['text2']
+    combine = do_something(text1,text2)
+    result = {
+        "output": combine
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
