15644186433129443c68951d6a84f1b6c0d97879
Alon Cohen <alon@placer.io>
2023-08-03 11:23:06 +0300

    Update app.py
    app update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from whatstk import df_from_txt_whatsapp
-
+from werkzeug.utils import secure_filename
+import os
 from flask import *
 
 app = Flask(__name__)
+
+EXPORT_PATH = '/home/aloncohen/whatsapp-analyzer/data'
 
 
 @app.route('/')
@@ -14,9 +17,10 @@ def main():
 def success():
     if request.method == 'POST':
         f = request.files['file']
-        f.save('data/'+f.filename)
-        print('data/'+f.filename)
-        df = df_from_txt_whatsapp('data/'+f.filename)
+
+        f.save(os.path.join(EXPORT_PATH, secure_filename(f.filename)))
+        print(os.path.join(EXPORT_PATH, secure_filename(f.filename)))
+        df = df_from_txt_whatsapp(os.path.join(EXPORT_PATH, secure_filename(f.filename)))
         return df.to_html()
         # return render_template("Acknowledgement.html", name=f.filename)
 
