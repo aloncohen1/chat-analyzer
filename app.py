c7a41d71fe2537445cd2f43bb4df883708aad13f
Alon Cohen <alon@placer.io>
2023-08-07 01:29:31 +0300

    update whole napplication
    update whole napplication

--- app.py
+++ app.py
@@ -1,41 +1,53 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from whatstk import df_from_txt_whatsapp
+from hashlib import md5
 from werkzeug.utils import secure_filename
-import os
+
+import pandas as pd
 
 app = Flask(__name__)
 
-# EXPORT_PATH =   '/Users/aloncohen/private_repos/whatsapp-analyzer/data'
-EXPORT_PATH = '/home/aloncohen/whatsapp-analyzer/data'
+UPLOAD_FOLDER = 'data'
+ALLOWED_EXTENSIONS = {'txt'}
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(os.path.join(EXPORT_PATH, secure_filename(f.filename)))
-        return redirect(url_for('display_file', content=os.path.join(EXPORT_PATH, secure_filename(f.filename))))
-    return render_template('index.html')
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], md5(file.filename.encode()).hexdigest()+'.txt')
+            file.save(filename)
+            return redirect(url_for('view_data', filename=md5(file.filename.encode()).hexdigest()+'.txt'))
+
+    return render_template('upload.html')
 
 
-@app.route('/display')
-def display_file():
+@app.route('/view/<filename>')
+def view_data(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
     try:
-        f_path = request.args.get('content', '')
-
-        df = df_from_txt_whatsapp(f_path)
-        os.system(f'rm {f_path}')
-        #return render_template(df.to_html())
-        return render_template('display.html', tables=[df.to_html(classes='data', header="true")])
+        data = df_from_txt_whatsapp(file_path)
+        os.system(f'rm {file_path}')
+        return render_template('view_data.html', data=data.to_html(classes='data'))
     except Exception as e:
-        return f'<html><body><h1>ERROR: {e}</h1></body></html>'
-    # return render_template('display.html', tables=[df.to_html(classes='data', header="true")])
+        return f"Error: {e}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
